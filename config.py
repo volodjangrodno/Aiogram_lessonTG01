1b0d7d0bb73091d7bdee2f6f2e6cfd19e4947848
ULADZIMIR YANCHANKA <164370799+volodjangrodno@users.noreply.github.com>
2024-08-15 14:20:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = 'your TOKEN for bot from Telegram'
 WEATHER_API_KEY = 'your API KEY from openweathermap.org'
 THE_CAT_API_KEY = 'your API KEY from thecatapi.com'
+NASA_API_KEY = 'your API KEY from api.nasa.gov'
