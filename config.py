f27e42bc12a7904dec38c9e3f8cbfdb627493e44
volodjangrodno <parthnersclub.ru@gmail.com>
2024-08-15 14:22:29 +0300

    TG05klass nasa.py and config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-TOKEN = 'your TOKEN for bot from Telegram'
-WEATHER_API_KEY = 'your API KEY from openweathermap.org'
-THE_CAT_API_KEY = 'your API KEY from thecatapi.com'
+TOKEN = '7429932424:AAHmXMn0ySjlzVGchYheqpC67nR0rfAAUJg'
+WEATHER_API_KEY = 'a1db76d80a9c6347b531e64c7713f406'
+THE_CAT_API_KEY = 'live_Tx6kHcRcOeDfDEbuXLDHVRgdH2Rky8WUCJgzpoTZyDol89GLayMOrbzgB4LQb7nt'
+NASA_API_KEY = 'r3JT2jEyT6OyHNp7oW3xlTYsmqdc2tduKhBPuzqo'
+
